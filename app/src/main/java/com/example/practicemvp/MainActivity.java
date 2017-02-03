b344a7969bc0bc161347c876b997cc70c202d4f9
DESKTOP-MR4L1RM\kth919 <kth9193@gmail.com>
2017-02-03 21:28:33 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/example/practicemvp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.practicemvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// MVP패턴을 적용한 안드로이드 샘플
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

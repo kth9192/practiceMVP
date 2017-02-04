e8f27a86a9ebbcb11d79de9d29ec851e34fcd2af
DESKTOP-MR4L1RM\kth919 <kth9193@gmail.com>
2017-02-04 20:25:56 +0900

    TDD 연습1

---
+++ app/src/androidTest/java/com/example/practicemvp/EspressoTest.java
@@ -0,0 +1,45 @@
+package com.example.practicemvp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressBack;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by kth919 on 2017-02-04.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+            new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public  void changeText(){
+
+        //원본 텍스트 출력 체크
+        onView(withId(R.id.top_text)).check(matches(withText("Hello World!")));
+        //에디트 텍스트로 텍스트 보내기
+        onView(withContentDescription("text_field")).perform(typeText("Whoa Espresso is easy!"), pressBack());
+        //체인지 버튼 클릭
+        onView(withId(R.id.change_button)).perform( click());
+        //
+        onView(withId(R.id.top_text)).check(matches(withText("Whoa Espresso is easy!")));
+
+
+    }
+}
--- app/src/main/java/com/example/practicemvp/MainActivity.java
+++ app/src/main/java/com/example/practicemvp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.practicemvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 // MVP패턴을 적용한 안드로이드 샘플
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button changeButton = (Button) findViewById(R.id.change_button);
+        final TextView textView = (TextView) findViewById(R.id.top_text);
+        final EditText textField = (EditText) findViewById(R.id.editText);
+
+        changeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                textView.setText(textField.getText());
+            }
+        });
     }
+
 }
